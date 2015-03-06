62d19e308c13d6bc642eafed8f9ae339d1e0cc45
Gregory Haskins <greg@greghaskins.com>
2015-03-06 17:10:06 -0500

    simple pass-through GET proxy

--- main.go
+++ main.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 const listeningAddress = ":7070"
 
 func main() {
 	fmt.Printf("Listening on %s ...\n", listeningAddress)
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/get", handleGet)
 	http.ListenAndServe(listeningAddress, nil)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello, my name is JSONpea"))
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	encodedUrl := r.URL.Query().Get("url")
+
+	remoteUrl, err := url.QueryUnescape(encodedUrl)
+	if err != nil {
+		log.Printf("Error parsing URL: %s\n", remoteUrl)
+		return
+	}
+
+	data, err := fetchBody(remoteUrl)
+	if err != nil {
+		log.Printf("Error fetching <%s>: %s\n", remoteUrl, err)
+		return
+	}
+
+	w.Write(data)
+}
+
+func fetchBody(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return contents, nil
 }
